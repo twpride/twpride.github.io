8227fbcbc31fb60ccd42d0b7f1d8457ad2d35e5b
howard <twpride@gmail.com>
2021-03-03 19:19:46 -0800

    push

--- components/layout.js
+++ components/layout.js
@@ -2,40 +2,16 @@
 import Head from 'next/head'
 import Navbar from './navbar'
 import Footer from './footer'
-import { useEffect, useState } from 'react'
 
 export default function Layout({ children }) {
 
-  const [TTvis, setTTvis] = useState(false)
-
-  useEffect(() => {
-
-    function copyTextToClipboard(text) {
-      var textArea = document.createElement("textarea");
-      textArea.value = text;
-      document.body.appendChild(textArea);
-      // textArea.focus();
-      textArea.select();
-      try {
-        var successful = document.execCommand('copy');
-        var msg = successful ? 'successful' : 'unsuccessful';
-        console.log('Copying text command was ' + msg);
-      } catch (err) {
-        console.log('Oops, unable to copy');
-      }
-      document.body.removeChild(textArea);
-      setTTvis(false)
-    }
-
-    document.getElementById("email").addEventListener("click", () => copyTextToClipboard('twpride@gmail44.com'));
-  }, [])
-
-
-
-
-
   return <>
     <Head>
+      <meta property="og:title" content="Portfolio" />
+      <meta property="og:description" content="Howard Hwang" />
+      <meta property="og:url" content="https://twpride.github.io" />
+      <meta property="og:image" content="https://raw.githubusercontent.com/twpride/tangram/master/demo/collision_opt.gif" />
+
       <meta name="theme-color" content="#90cdf4" />
       <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
       <link rel="icon" href="/favicon.ico" />
